e574c5c0037529c55a74bdde1b681c40c07debb2
vamsidevalla <vammichanti40312@gmail.com>
2021-09-21 16:37:08 +0530

    adding petal paths and colors

---
+++ fm_moviesToFlowers/petalsAndColors.js
@@ -0,0 +1,14 @@
+export const petals = [
+  'M0 0 C50 50 50 100 0 100 C-50 100 -50 50 0 0',
+  'M-35 0 C-25 25 25 25 35 0 C50 25 25 75 0 100 C-25 75 -50 25 -35 0',
+  'M0,0 C50,40 50,70 20,100 L0,85 L-20,100 C-50,70 -50,40 0,0',
+  'M0 0 C50 25 50 75 0 100 C-50 75 -50 25 0 0',
+];
+
+export const colors = {
+  Action: '#ffc8f0',
+  Comedy: '#cbf2bd',
+  Animation: '#afe9ff',
+  Drama: '#ffb09e',
+  Other: '#FFF2B4',
+};
