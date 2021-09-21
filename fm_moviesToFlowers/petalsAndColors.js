be7d226697f33f69fb32eec9b6d9b940f711e3f4
vamsidevalla <vammichanti40312@gmail.com>
2021-09-21 19:25:45 +0530

    renaming paths and  colors

--- fm_moviesToFlowers/petalsAndColors.js
+++ fm_moviesToFlowers/petalsAndColors.js
@@ -1,11 +1,11 @@
-export const petals = [
+export const petalPaths = [
   'M0 0 C50 50 50 100 0 100 C-50 100 -50 50 0 0',
   'M-35 0 C-25 25 25 25 35 0 C50 25 25 75 0 100 C-25 75 -50 25 -35 0',
   'M0,0 C50,40 50,70 20,100 L0,85 L-20,100 C-50,70 -50,40 0,0',
   'M0 0 C50 25 50 75 0 100 C-50 75 -50 25 0 0',
 ];
 
-export const colors = {
+export const petalColors = {
   Action: '#ffc8f0',
   Comedy: '#cbf2bd',
   Animation: '#afe9ff',
